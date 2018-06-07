1c82cd8643181fec1cdf05ce2158c72195f97350
Lakshmi Pramod <Lakshmi Pramod@DESKTOP-UH9BKI5>
2018-06-07 15:01:33 +0530

    test commit

---
+++ practice/src/main/java/sel/practice/A2.java
@@ -0,0 +1,14 @@
+package sel.practice;
+
+public class A2 {
+
+	public A2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ practice/src/main/java/sel/practice/Google.java
@@ -0,0 +1,36 @@
+package sel.practice;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+ 
+public class Google {
+ 
+public static void main(String[] args) {
+ 
+System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+ 
+// Initialize browser
+WebDriver driver=new ChromeDriver();
+ 
+// Open google
+driver.get("http://www.google.com");
+ 
+// Maximize browser
+ 
+driver.manage().window().maximize();
+
+
+
+try {
+	Thread.sleep(2000);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+ 
+
+driver.quit();
+}
+ 
+}
